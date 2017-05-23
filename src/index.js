9106e5bb7b1272d0bb4fe0c5a5288f2732e6193c
snaipi91 <snaipi91@rambler.ru>
2017-05-23 19:02:49 +0300

    add devTools redux

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ import reducers from './reducers/index';
 // };
 
 // store
-const store = createStore(reducers);
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.dispatch({
     type: 'ADD_DATA',
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,5 @@ import { combineReducers } from 'redux';
 import widgetWeather from './widgetWeather';
 
 export default combineReducers({
-    widgetWeather
+    widgetWeather,
 })
